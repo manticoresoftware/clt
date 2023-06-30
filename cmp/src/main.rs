71217297da15e4246f665994902eb8b0a36dd235
Don Hardman <don.hardman@muvon.io>
2023-06-30 09:11:18 +0000

    Add support of regex patterns through predefined file

--- cmp/src/main.rs
+++ cmp/src/main.rs
@@ -14,9 +14,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::env;
+use std::path::Path;
 use regex::Regex;
 
 const COMMAND_PREFIX: &str = "––– input –––";
@@ -29,6 +31,14 @@ fn main() {
 		std::process::exit(1);
 	}
 
+	let file_name: String = String::from(".patterns");
+	let file_path = Path::new(&file_name);
+
+	let pattern_matcher = PatternMatcher::new(match file_path.exists() {
+		true => Some(file_name),
+		false => None,
+	}).unwrap();
+
 	let file1 = File::open(&args[1]).unwrap();
 	let file2 = File::open(&args[2]).unwrap();
 	let mut reader1 = BufReader::new(file1);
@@ -37,7 +47,6 @@ fn main() {
 	let mut line1 = String::new();
 	let mut line2 = String::new();
 
-	let pattern_regex = Regex::new(r"#!/(.+?)/!#").unwrap();
 	let mut files_have_diff = false;
 	let mut is_line1_output = false;
 	let mut is_line2_output = false;
@@ -103,22 +112,7 @@ fn main() {
 
 		// Do the logic only for output
 		if is_line1_output && is_line2_output {
-			let mut has_diff: bool = false;
-			let mut match_count = 0;
-			for capture in pattern_regex.captures_iter(&line1) {
-				match_count += 1;
-				let pattern = capture.get(1).unwrap().as_str();
-				let pattern_regex = Regex::new(pattern).unwrap();
-				if !pattern_regex.is_match(&line2) {
-					has_diff = true;
-					break;
-				}
-			}
-
-			// If no matches but lines are different – also add to diff
-			if match_count == 0 && &line1 != &line2 {
-				has_diff = true;
-			}
+			let has_diff: bool = pattern_matcher.has_diff(line1.to_string(), line2.to_string());
 
 			if has_diff {
 				eprintln!("- {}", line1.trim());
@@ -138,5 +132,84 @@ fn main() {
 
 	if files_have_diff {
 		std::process::exit(1);
+	}
+}
+
+struct PatternMatcher {
+	config: HashMap<String, String>,
+	val_regex: Regex,
+	key_regex: Regex,
+}
+
+impl PatternMatcher {
+	/// Initialize struct by using file name of the variables description for patterns
+	/// If the option is none, we just will have empty map of keys for pattersn
+	/// And in that case we will use only raw regexes to validate
+	fn new(file_name: Option<String>) -> Result<Self, Box<dyn std::error::Error>> {
+		let config = match file_name {
+			Some(file_name) => Self::parse_config(file_name)?,
+			None =>  HashMap::new(),
+		};
+
+		let key_regex = Regex::new(r"%\{[A-Z]{1}[A-Z_0-9]*\}")?;
+		let val_regex = Regex::new(r"#!/(.+?)/!#")?;
+
+		Ok(Self { config, key_regex, val_regex })
+	}
+
+	/// Validate line from .rec file and line from .rep file
+	/// by using open regex patterns and matched variables
+	/// and return true or false in case if we have diff or not
+	fn has_diff(&self, rec_line: String, rep_line: String) -> bool {
+		// 1. We replace all variables matches to raw regexp
+		let rec_line = self.replace_vars_to_patterns(rec_line);
+
+		// 2. We go through the line and validate it as expanded raw regex in it without any vars
+		let mut match_count = 0u8;
+		for capture in self.val_regex.captures_iter(&rec_line) {
+			match_count = match_count + 1;
+			let pattern = capture.get(1).unwrap().as_str();
+			let pattern_regex = Regex::new(pattern).unwrap();
+			if !pattern_regex.is_match(&rep_line) {
+				return true;
+			}
+		}
+
+		match_count == 0 && rec_line != rep_line
+	}
+
+  /// Helper function that go through matched variable patterns in line
+	/// And replace it all with values from our parsed config
+	/// So we have raw regex to validate as an output
+	fn replace_vars_to_patterns(&self, line: String) -> String {
+    let result = self.key_regex.replace_all(&line, |caps: &regex::Captures| {
+			let matched = &caps[0];
+			let key = matched[2..matched.len() - 1].to_string();
+			self.config.get(&key).unwrap_or(&matched.to_string()).clone()
+		});
+
+		result.into_owned()
+	}
+
+	/// Helper to parse the variables into config map when we pass path to the file
+	fn parse_config(file_name: String) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+		let mut config: HashMap<String, String> = HashMap::new();
+
+		let file_path = Path::new(&file_name);
+		let file = File::open(&file_path)?;
+		let reader = BufReader::new(file);
+
+		for line in reader.lines() {
+			let line = line?.trim().to_string();
+			let parts: Vec<&str> = line.split_whitespace().collect(); // adjust this based on how your file is structured
+			if parts.len() == 2 {
+				config.insert(
+					parts[0].trim().to_string(),
+					format!("#!/{}/!#", parts[1].trim())
+				);
+			}
+		}
+
+		Ok(config)
 	}
 }
