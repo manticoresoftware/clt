c777ae6d23090a1279c74f80ddf549555b9a03d5
Don Hardman <don.hardman@muvon.io>
2023-09-28 19:50:38 +0700

    Rewrite the code for comparing by detecting absolute path first and update binaries

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -12,28 +12,30 @@ pub const BLOCK_REGEX: &str = r"(?m)^––– block: ([\.a-zA-Z0-9\-\/\_]+) –
 /// - contains expanded blocks with --- block: file –––
 /// TODO: - contains expanded patterns from .patterns file into raw regex ()
 pub fn compile(rec_file_path: &str) -> Result<String> {
-  let input_file = File::open(rec_file_path)?;
+	let input_file = File::open(rec_file_path)?;
 	let input_dir = Path::new(rec_file_path).parent().unwrap_or_else(|| Path::new(""));
-  let reader = BufReader::new(input_file);
-  let mut result = String::new();
+	let reader = BufReader::new(input_file);
+	let mut result = String::new();
 
-  let re = Regex::new(BLOCK_REGEX)?;
-  for line in reader.lines() {
-    let line = line.unwrap();
-    if let Some(caps) = re.captures(&line) {
-      let block_name = caps.get(1).map_or("", |m| m.as_str());
-      let block_path = input_dir.join(format!("{}.recb", block_name));
-      let block_content = read_to_string(block_path)?;
-      result.push_str(block_content.trim());
-      result.push('\n');
-      continue;
-    }
+	let re = Regex::new(BLOCK_REGEX)?;
+	for line in reader.lines() {
+		let line = line.unwrap();
+		if let Some(caps) = re.captures(&line) {
+			let block_name = format!("{}.recb", caps.get(1).map_or("", |m| m.as_str()));
+			let relative_path = Path::new(&block_name);
+			let block_path = input_dir.join(relative_path);
+			let absolute_path = std::fs::canonicalize(block_path)?;
+			let block_content = read_to_string(absolute_path)?;
+			result.push_str(block_content.trim());
+			result.push('\n');
+			continue;
+		}
 
-    result.push_str(&line);
-    result.push('\n');
-  }
+		result.push_str(&line);
+		result.push('\n');
+	}
 
-  Ok(result)
+	Ok(result)
 }
 
 
