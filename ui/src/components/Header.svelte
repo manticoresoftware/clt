d46ccf6ab61517ec5c5ba8915fc92d624682ece0
Don Hardman <don.hardman@muvon.io>
2025-06-19 22:56:07 +0700

    RENAME

--- ui/src/components/Header.svelte
+++ ui/src/components/Header.svelte
@@ -74,7 +74,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
     </svg>
-    Manticore Test Editor
+    CLT Editor
   </div>
 
   <div class="docker-image-container">
