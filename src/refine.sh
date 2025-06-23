eafc45c33216aead1885aa218116110ba94cdb7d
Don Hardman <don.hardman@muvon.io>
2025-06-23 16:10:15 +0700

    fix

--- src/refine.sh
+++ src/refine.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+# ! We are handling exit codes so we cannot use set -e here
 source "$PROJECT_DIR/lib/rec.sh"
 source "$PROJECT_DIR/lib/argument.sh"
 
@@ -43,3 +43,4 @@ while [[ $# -gt 0 ]]; do
 done
 
 refine "$docker_image" "$record_file"
+exit $?
